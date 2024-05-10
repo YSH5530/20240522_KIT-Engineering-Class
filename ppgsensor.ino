e182960ca74612ed302bfb22613e8995ad51b0a7
YSH5530 <ysh5530@hanmail.net>
2024-05-10 15:54:23 +0900

    first_derivative.ino
    Practice_3

--- ppgsensor.ino
+++ ppgsensor.ino
@@ -1,23 +1,43 @@
-int PulseSensorPurplePin = 0;
-int Signal;
+#define ts 0.001 * 0.001
 
-void setup() {
-   Serial.begin(9600);
+float PulseSensorPurplePin = 0;
+unsigned long tCount = 0;
+unsigned long tCountPre = 0;
+
+float Signal = 0;
+float Signal_pre = 0;
+float diff_Signal = 0;
+float dt = 0;
+
+
+void setting() {
+  tCount = micros();
+  Signal = analogRead(PulseSensorPurplePin);
+  dt = (float)(tCount - tCountPre) * ts;
+}
+
+void saveStates() {
+  Signal_pre = Signal;
+  tCountPre = tCount;
+}
+void firstDerivative() {
+  diff_Signal = (Signal - Signal_pre) / dt;
+}
+
+void getData() {
+  setting();
+  firstDerivative();
+  saveStates();
+}
+
+void setup()
+{
+  Serial.begin(9600);
 }
 
 void loop() {
-   Signal = analogRead(PulseSensorPurplePin);
-   Serial.println(Signal);
+  getData();
+  Serial.print(Signal);
+  Serial.print(" ");
+  Serial.println(diff_Signal);
 }
-
-
-int LED = 13;
-int Threshold = 450;
-
-  pinMode(LED,OUTPUT);
-
-   if(Signal > Threshold){
-     digitalWrite(LED,HIGH);
-   } else {
-     digitalWrite(LED,LOW);
-   }
