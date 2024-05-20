69cc2053fedee6bad9bebf74263582f714da42d6
YSH5530 <ysh5530@hanmail.net>
2024-05-20 22:37:09 +0900

    Update Practice_4.ino

--- Practice_4.ino
+++ Practice_4.ino
@@ -1,31 +1,54 @@
-#include <PulseSensorPlayground.h>
+#define ts 0.001 * 0.001
 
-int PulseWire = 0; 
-int LED = 13; 
-int Threshold = 450;       
-                               
-PulseSensorPlayground pulseSensor; 
+float PulseSensorPurplePin = 0;
+unsigned long tCount = 0;
+unsigned long tCountPre = 0;
 
-void setup() {   
+float Signal = 0;
+float diff_Signal = 0;
+float dt = 0;
 
-  Serial.begin(9600);  
+float Pre_Filtered_Signal;
+float Filtered_Signal;
+float Pre_Signal = 0;
+float Pre_diff = 0;
 
-  pulseSensor.analogInput(PulseWire);   
-  pulseSensor.setThreshold(Threshold);   
+const float a2 = 0.7265;
+const float b1 = 0.1367;
+const float b2 = 0.1367;
 
-   if (pulseSensor.begin()) {
-    Serial.println("start");
-  }
+void setting() {
+  tCount = micros();
+  Signal = analogRead(PulseSensorPurplePin);
+  dt = (float)(tCount - tCountPre) * ts;
+}
+
+void saveStates() {
+  Pre_Signal = Signal;
+  tCountPre = tCount;
+}
+void firstDerivative() {
+  diff_Signal = (Signal - Pre_Signal) / dt;
+}
+
+void getData() {
+  setting();
+  firstDerivative();
+  saveStates();
+}
+
+void setup()
+{
+  Serial.begin(9600);
 }
 
 void loop() {
-
-if (pulseSensor.sawStartOfBeat()) {           
-int myBPM = pulseSensor.getBeatsPerMinute(); 
-                                            
- Serial.print("BPM: ");                      
- Serial.println(myBPM);                      
- }
-}
-
+  getData();
+  Pre_Filtered_Signal = Filtered_Signal;
+  Pre_diff = diff_Signal;
   
+  Filtered_Signal = a2 * Pre_Filtered_Signal + b1 * diff_Signal + b2 * Pre_diff;
+  Serial.print(Signal);
+  Serial.print(" ");
+  Serial.println(Filtered_Signal);
+}
