935c2c1cb6fb11a22f1f11fccda55a6ec87cc8d8
YSH5530 <ysh5530@hanmail.net>
2024-05-20 19:51:49 +0900

    Add files via upload

--- Practice_2.ino
+++ Practice_2.ino
@@ -1,26 +1,25 @@
 int PulseSensorPurplePin = 0;
 int Signal;
-int LED = 13;
-int Threshold = 450;
-int LED_H = 0;
+float Pre_Filtered_Signal;
+float Filtered_Signal;
+float Pre_Signal = 0;
+
+const float a2 = 0.7265;
+const float b1 = 0.1367;
+const float b2 = 0.1367;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED,OUTPUT);
 }
 
 void loop() {
   Signal = analogRead(PulseSensorPurplePin);
+  Pre_Filtered_Signal = Filtered_Signal;
+  Pre_Signal = Signal;
+  
+  Filtered_Signal = a2 * Pre_Filtered_Signal + b1 * Signal + b2 * Pre_Signal;
+  
   Serial.print(Signal);
   Serial.print(" ");
-
-  if(Signal > Threshold) {
-    digitalWrite(LED,HIGH);
-    LED_H = 1023;
-  }
-  else {
-    digitalWrite(LED,LOW);
-    LED_H = 0;
-  }
-  Serial.println(LED_H);
+  Serial.println(Filtered_Signal);
 }
