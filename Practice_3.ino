31da03061f1b6a5280b0d9b95d9c4bd98be4d05a
YSH5530 <ysh5530@hanmail.net>
2024-05-20 22:36:00 +0900

    Update Practice_3.ino

--- Practice_3.ino
+++ Practice_3.ino
@@ -1,43 +1,38 @@
-#define ts 0.001 * 0.001
+int PulseSensorPurplePin = 0;
+int Signal;
+float Pre_Filtered_Signal;
+float Filtered_Signal;
+float Pre_Signal = 0;
 
-float PulseSensorPurplePin = 0;
-unsigned long tCount = 0;
-unsigned long tCountPre = 0;
+const float a2 = 0.7265;
+const float b1 = 0.1367;
+const float b2 = 0.1367;
 
-float Signal = 0;
-float Signal_pre = 0;
-float diff_Signal = 0;
-float dt = 0;
+int LED = 13;
+int Threshold = 360;
+int LED_H = 0;
 
-
-void setting() {
-  tCount = micros();
-  Signal = analogRead(PulseSensorPurplePin);
-  dt = (float)(tCount - tCountPre) * ts;
-}
-
-void saveStates() {
-  Signal_pre = Signal;
-  tCountPre = tCount;
-}
-void firstDerivative() {
-  diff_Signal = (Signal - Signal_pre) / dt;
-}
-
-void getData() {
-  setting();
-  firstDerivative();
-  saveStates();
-}
-
-void setup()
-{
+void setup() {
   Serial.begin(9600);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  getData();
-  Serial.print(Signal);
+  Signal = analogRead(PulseSensorPurplePin);
+  Pre_Filtered_Signal = Filtered_Signal;
+  Pre_Signal = Signal;
+  
+  Filtered_Signal = a2 * Pre_Filtered_Signal + b1 * Signal + b2 * Pre_Signal;
+  
+  Serial.print(Filtered_Signal);
   Serial.print(" ");
-  Serial.println(diff_Signal);
+
+  if(Filtered_Signal > Threshold) {
+    digitalWrite(LED,HIGH);
+    LED_H = 400;
+  } else {
+    digitalWrite(LED,LOW);
+    LED_H = 320;
+  }
+  Serial.println(LED_H);
 }
