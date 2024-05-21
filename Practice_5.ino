2ef781746b0b9c5caa41383f3c4611de875ce090
YSH5530 <ysh5530@hanmail.net>
2024-05-21 14:24:17 +0900

    Update Practice_5.ino

--- Practice_5.ino
+++ Practice_5.ino
@@ -1,17 +1,15 @@
+#include <filter_lib.h>
+
+lowpass_filter lowpassFilter(10);
+
 float PulseSensorPurplePin = 0;
 unsigned long lastBeatTime = 0;
 
 int BPM = 0;
 int Signal;
-float Pre_Filtered_Signal;
 float Filtered_Signal;
-float Pre_Signal = 0;
 
-int Threshold = 350;
-
-const float a2 = 0.7265;
-const float b1 = 0.1367;
-const float b2 = 0.1367;
+int Threshold = 360;
 
 void setup() {
   Serial.begin(9600);
@@ -19,12 +17,9 @@ void setup() {
 
 void loop() {
   Signal = analogRead(PulseSensorPurplePin);
-  Pre_Filtered_Signal = Filtered_Signal;
-  Pre_Signal = Signal;
-  
-  Filtered_Signal = a2 * Pre_Filtered_Signal + b1 * Signal + b2 * Pre_Signal;
-  
-  if (Signal > Threshold) {
+  Filtered_Signal = lowpassFilter.filter(Signal);
+
+  if (Filtered_Signal > Threshold) {
     unsigned long currentTime = millis();
     if (currentTime - lastBeatTime > 300) {
       unsigned long beatInterval = currentTime - lastBeatTime;
